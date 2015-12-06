a7d7e6480149c88b9515165d5b6ff3ef01eaa20d
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-12-06 04:06:32 +0000

    Write babel config to a temporary babelrc file when building modules
    Fixes #13

--- src/commands/build-module.js
+++ src/commands/build-module.js
@@ -1,14 +1,33 @@
+import fs from 'fs'
 import path from 'path'
+
+import temp from 'temp'
+
+import debug from '../debug'
 import exec from '../exec'
+import getUserConfig from '../getUserConfig'
 
-require('./clean-module')
+export default function(args) {
+  require('./clean-module')
 
-let src = path.resolve('src')
-let lib = path.resolve('lib')
+  let src = path.resolve('src')
+  let lib = path.resolve('lib')
 
-console.log('nwb: build-module')
-exec(
-  'babel',
-  [src, '--out-dir', lib],
-  {cwd: path.join(__dirname, '..')}
-)
+  let babelArgs = [src, '--out-dir', lib]
+
+  // Write any user babel config to a temporary file to point babel at via the
+  // babelrc option.
+  let userConfig = getUserConfig(args)
+  if (userConfig.babel) {
+    var babelrc = temp.openSync()
+    debug('writing babelrc to %s', babelrc.path)
+    fs.writeSync(babelrc.fd, JSON.stringify(userConfig.babel))
+    fs.closeSync(babelrc.fd)
+    babelArgs = [...babelArgs, '--babelrc', babelrc.path]
+  }
+
+  console.log('nwb: build-module')
+  exec('babel', babelArgs, {cwd: path.join(__dirname, '..')})
+
+  temp.cleanupSync()
+}
