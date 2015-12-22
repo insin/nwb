641ca0f4bf6aa6d2c9ba9edf69b1323684f8fe8f
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-12-22 12:37:46 +0000

    Undo some premature modularisation

--- src/commands/serve-react-app.js
+++ src/commands/serve-react-app.js
@@ -1,7 +1,16 @@
-import createServeReactAppConfig from '../createServeReactAppConfig'
+import path from 'path'
+
 import serveReact from '../serveReact'
 
 export default function(args, cb) {
   console.log('nwb: serve-react-app')
-  serveReact(args, createServeReactAppConfig(), cb)
+  serveReact(args, {
+    entry: path.resolve('src/index.js'),
+    output: {
+      path: path.resolve('public/build'),
+      filename: 'app.js',
+      publicPath: '/build/'
+    },
+    staticPath: path.resolve('public')
+  }, cb)
 }
