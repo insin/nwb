01897e9aafc2e0598f2183e81297778cb7840b94
ntwcklng <ntwcklng@gmail.com>
2016-12-11 10:02:24 +0100

    add src/interactiveNew.js

---
+++ src/interactiveNew.js
@@ -0,0 +1,27 @@
+import inquirer from 'inquirer'
+import {PROJECT_TYPES} from './constants'
+
+export default function interactiveNew_(args) {
+  const prompts = [
+    {
+      type: 'list',
+      name: 'projectType',
+      message: 'Select a Project Type',
+      choices: PROJECT_TYPES.map(type => {
+        const displayName = type.replace('-', ' ').split(' ').map(x => x[0].toUpperCase() + x.slice(1)).join(' ')
+        return {
+          name: displayName,
+          value: type
+        }
+      })
+    },
+    {
+      type: 'input',
+      name: 'projectName',
+      message: 'Enter a Project Name',
+    }
+  ]
+  return inquirer
+    .prompt(prompts)
+    .then(answers => Object.assign({}, args, answers))
+}
