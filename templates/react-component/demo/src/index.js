1fd97c1de5dcd033fc28ce4c4061c44447fcc027
Ade Viankakrisna Fadlil <viankakrisna@gmail.com>
2017-04-28 03:31:28 +0700

    update react templates to use es6 class as default

--- templates/react-component/demo/src/index.js
+++ templates/react-component/demo/src/index.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import {render} from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import Component from '../../src'
+import Component from '../../src';
 
-let Demo = React.createClass({
-  render() {
-    return <div>
-      <h1>{{name}} Demo</h1>
-      <Component/>
-    </div>
-  }
-})
+class Demo extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>{{ name }} Demo</h1>
+				<Component />
+			</div>
+		);
+	}
+}
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector('#demo'));
