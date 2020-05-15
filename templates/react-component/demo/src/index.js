072ea4e9ebed544ba9bc7ea1af40cbcbcc778437
Ivan Jeremic <ivan.jeremic@outlook.com>
2020-05-15 13:31:29 +0200

    Update demo/index.js to use functional component

--- templates/react-component/demo/src/index.js
+++ templates/react-component/demo/src/index.js
@@ -1,15 +1,15 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React from "react";
+import { render } from "react-dom";
 
 import Example from '../../src'
 
-export default class Demo extends Component {
-  render() {
-    return <div>
+export default function Demo() {
+  return (
+    <div>
       <h1>{{name}} Demo</h1>
       <Example/>
     </div>
-  }
+  );
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
