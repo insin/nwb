1fd82f6db645486c7e96b7754d394846fcabd116
kaiwenqin <74439654+kaiwenqin@users.noreply.github.com>
2021-01-15 10:14:45 +0800

    fix react-app template logo issue
    change require('./react.svg') to require('./react.svg').default

--- templates/react-app/src/App.js
+++ templates/react-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <h2>Welcome to <span className="App-react">React</span></h2>
       </div>
       <div className="App-instructions App-flex">
-        <img className="App-logo" src={require('./react.svg')}/>
+        <img className="App-logo" src={require('./react.svg').default}/>
         <p>Edit <code>src/App.js</code> and save to hot reload your changes.</p>
       </div>
     </div>
